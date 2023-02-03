c53115da6fa4d8e8b0735e46da53aa9069c847a4
Жанна Ивашова <zhannaklepikova90@gmail.com>
2023-02-03 16:02:07 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index (request):
+    return HttpResponse ('Главная страница социальной сети YATUBE')
+
+def group_posts (request, slug): 
+    return HttpResponse (f'Страница для размещения постов {slug}')   
